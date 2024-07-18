c162dbb4807ee357b8aaaf713a9f49a4c30af23e
Krishna Chamarthy <chamarthysr@gmail.com>
2024-07-18 18:26:22 +0530

    18/7/24

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Navbar.css"
 import Logo from "../../assets/Logo.png"
+import Profile_icon from "../../assets/boy.png"
+import Bell_icon from "../../assets/bell.png"
 
 const Navbar = () => {
   return (
@@ -9,6 +11,18 @@ const Navbar = () => {
             <img src={Logo} alt="" className='logo'/>
             <div className="seperator"></div>
             <p>Student Dashboard</p>
+        </div>
+        <div className="navbar-right">
+          <div className="navbar-bell-icon">
+            <img src={Bell_icon} alt="" className='bell-icon'/>
+            <div className="dot">3</div>
+          </div>
+          <div className="seperator"></div>
+          <div className="profile-info">
+            <p>Krishna Srimanth Chamarthy</p>
+            <span>[Student]</span>
+          </div>
+          <img src={Profile_icon} alt="" className='profile-icon'/>
         </div>
     </div>
   )
