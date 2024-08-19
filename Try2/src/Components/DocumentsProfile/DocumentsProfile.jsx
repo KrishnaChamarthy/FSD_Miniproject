9b1e46eaf89f32971954e1b2f3ab65b8c768e64c
Krishna Chamarthy <chamarthysr@gmail.com>
2024-08-19 21:11:05 +0530

    d

--- Try2/src/Components/DocumentsProfile/DocumentsProfile.jsx
+++ Try2/src/Components/DocumentsProfile/DocumentsProfile.jsx
@@ -3,7 +3,43 @@ import "./DocumentsProfile.css"
 
 const DocumentsProfile = () => {
   return (
-    <div>DocumentsProfile</div>
+    <div className='documents-profile'>
+      <div className="educational-profile-element">
+        <div className="element-title">Personal Documents</div>
+        <div className="document-table">
+          <tr>
+            <th>Document Name</th>
+            <th>Status</th>
+            <th>Upload Date</th>
+            <th>Uploaded By</th>
+          </tr>
+          <tr>
+            <td>SSC Marks Sheet</td>
+            <td>Approved</td>
+            <td>Date</td>
+            <td>Student</td>
+          </tr>
+          <tr>
+            <td>HSC Marks Sheet</td>
+            <td>Approved</td>
+            <td>Date</td>
+            <td>Student</td>
+          </tr>
+          <tr>
+            <td>Aadhar Card</td>
+            <td>Approved</td>
+            <td>Date</td>
+            <td>Student</td>
+          </tr>
+          <tr>
+            <td>Birth Certificate</td>
+            <td>Approved</td>
+            <td>Date</td>
+            <td>Student</td>
+          </tr>
+        </div>
+      </div>
+    </div>
   )
 }
 
