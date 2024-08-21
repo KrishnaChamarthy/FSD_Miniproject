833cdfdd66233318d86e97b74d6746d0f224d58f
Krishna Chamarthy <chamarthysr@gmail.com>
2024-08-21 20:18:32 +0530

    d

--- Try2/src/Components/AttendanceMain/AttendanceMain.jsx
+++ Try2/src/Components/AttendanceMain/AttendanceMain.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import "./AttendanceMain.css"
+import {Doughnut} from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
 
 const AttendanceMain = () => {
+  ChartJS.register(ArcElement, Tooltip, Legend);
+
+  const data = {
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100], 
+      backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+      hoverOffset: 5,
+    }]
+  };
+
+  const options = {
+    cutout: '70%',
+  };
+
   return (
     <div className='attendance-container'>
         <header>
@@ -18,6 +37,16 @@ const AttendanceMain = () => {
           <i class="bx bx-bell"></i>
         </div>
       </header>
+      <div className="attendance-body">
+        <div className="attendance-element attendance-chart">
+          <div className="chart-container">
+            <Doughnut data={data} options={options}/>
+          </div>
+        </div>
+        <div className="attendance-element attendance-summary">
+          dddd
+        </div>
+      </div>
     </div>
   )
 }
